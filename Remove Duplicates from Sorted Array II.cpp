b87835da42351bb6c170d2007dd73c6b97240bef
Ahmed El-gharbawy <136514345+Gharbawy1@users.noreply.github.com>
2024-01-10 22:47:15 +0200

    Update Remove Duplicates from Sorted Array II.cpp

---
+++ Remove Duplicates from Sorted Array II.cpp
@@ -0,0 +1,51 @@
+/*
+		2 - Remove Duplicates from Sorted Array II
+		> Solved by : Ahmed Elghrabawy
+		> problem link : https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/description/?envType=study-plan-v2&envId=top-interview-150
+	
+    */
+
+// #1 
+#include<vector>
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        //time O(N^2), space O(1)
+        // using 2 pointers techniqe
+        int flag = 0;
+        int cnt = 0;
+        int p1 = 0, p2 = 1;
+        while (p2 != nums.size())
+            if (nums[p1] == nums[p2] && flag == 0) {
+                flag = 1;
+                p2++;
+            }
+            else if (nums[p1] != nums[p2]) {
+                p1 = p2;
+                p2++;
+                flag = 0;
+            }
+            else {
+                nums.erase(nums.begin() + p2); // it make the time O(N^2)
+                // we can make a cnt to keep track for numbers i must remove and ret nums.size()-cnt
+            }
+        return nums.size();
+    }
+};
+// #2  another solution o(N)
+class Solution2 {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int i = 0;
+        // int ele= nums[0];
+        for (auto ele : nums)
+        {
+            if (i == 0 || i == 1 || nums[i - 2] != ele)
+            {
+                nums[i] = ele;
+                i++;
+            }
+        }
+        return i;
+    }
+};
